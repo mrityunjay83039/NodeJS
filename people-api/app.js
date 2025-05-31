8f868ffc77b1fc060449e8dfdfd2543f5b6c0c0d
Mrityunjay Kumar Mishra <Mrityunjay.Mishra@q3infotech.net>
2025-05-31 22:15:25 +0530

    created put method

--- people-api/app.js
+++ people-api/app.js
@@ -19,7 +19,26 @@ app.post("/api/people", (req, res)=>{
     return res.status(201).json({success:true, msg:"User added successfully", data:name});
 })
 
+// put method
+app.put("/api/people/:id", (req, res)=>{
+    const id = req.params.id;
+    const {name} = req.body;
 
+    let person = people.filter((person)=> person.id === Number(id));
+    if(!person){
+        return res.status(404).json({success:false, msg:"User not found"});
+    }else if(!name){
+        return res.status(400).json({success:false, msg:"Please add a name"});
+    }else{
+        const updatedPeople = people.map((user)=>{
+            if(user.id === Number(id)){
+                user.name = name;
+            }
+            return user;
+        })
+        return res.status(200).json({success:true, msg: "User updated successfully", data:updatedPeople});
+    }
+})
 
 app.listen(9000, ()=>{
     console.log("Server is running on port 5000");
