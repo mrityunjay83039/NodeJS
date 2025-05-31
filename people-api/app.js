bcc8f8746fe6ff476532a61abd1fc96d71d29b2f
Mrityunjay Kumar Mishra <Mrityunjay.Mishra@q3infotech.net>
2025-05-31 22:03:25 +0530

    created post method

--- people-api/app.js
+++ people-api/app.js
@@ -10,6 +10,15 @@ app.get("/api/people", (req, res)=>{
     return res.status(200).json({success:true, data:people});
 })
 
+// post method
+app.post("/api/people", (req, res)=>{
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({success:false, msg:"Please add a name"});
+    }
+    return res.status(201).json({success:true, msg:"User added successfully", data:name});
+})
+
 
 
 app.listen(9000, ()=>{
