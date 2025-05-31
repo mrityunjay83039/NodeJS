e52890f445ca94a24f2126241f4c5997b1ee1302
Mrityunjay Kumar Mishra <Mrityunjay.Mishra@q3infotech.net>
2025-05-31 11:36:43 +0530

    send response according to different routes

--- basic-http-server/app.js
+++ basic-http-server/app.js
@@ -1,12 +1,22 @@
 const http = require("http");
 
-const server = http.createServer((req, res)=>{
-
-    res.writeHead(200, {"content-type":"text/html"});  // status code and header
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" }); // status code and header
     res.write("<h1>Bsic app using http module.</h1>"); // write a response to the client
     res.end(); // this indicates the end of the response
-})
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.write("This is about page");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write(`<h1 style="color:red; text-align:center">Page Not Found!</h1>`);
+    res.end();
+  }
+});
 
-server.listen(5000, ()=>{
-    console.log('server is listening to 5000 port.');
-})
+server.listen(5000, () => {
+  console.log("server is listening to 5000 port.");
+});
